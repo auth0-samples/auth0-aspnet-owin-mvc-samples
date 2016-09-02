5160ccc07589582bcaca6a21e1a27fd32f27a6e7
jerri <jerri@DESKTOP-2OC92Q0>
2016-09-02 14:22:35 -0700

    Add Custom Login

---
+++ 02-Custom-Login/MvcApplication/MvcApplication/Controllers/AccountController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Auth0.AuthenticationApi;
+using Auth0.AuthenticationApi.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using MvcApplication.ViewModels;
+
+namespace MvcApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Login(string returnUrl = "/")
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginViewModel vm, string returnUrl = null)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    AuthenticationApiClient client =
+                        new AuthenticationApiClient(
+                            new Uri($"https://{ConfigurationManager.AppSettings["auth0:Domain"]}/"));
+
+                    var result = await client.AuthenticateAsync(new AuthenticationRequest
+                    {
+                        ClientId = ConfigurationManager.AppSettings["auth0:ClientId"],
+                        Scope = "openid",
+                        Connection = "Database-Connection", // Specify the correct name of your DB connection
+                        Username = vm.EmailAddress,
+                        Password = vm.Password
+                    });
+
+                    // Get user info from token
+                    var user = await client.GetTokenInfoAsync(result.IdToken);
+
+                    // Create claims principal
+                    var claimsIdentity = new ClaimsIdentity(new[]
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, user.UserId),
+                        new Claim(ClaimTypes.Name, user.FullName ?? user.Email),
+                        new Claim("http://schemas.microsoft.com/accesscontrolservice/2010/07/claims/identityprovider", "ASP.NET Identity", "http://www.w3.org/2001/XMLSchema#string")
+                    }, DefaultAuthenticationTypes.ApplicationCookie);
+
+                    // Sign user into cookie middleware
+                    AuthenticationManager.SignIn(new AuthenticationProperties { IsPersistent = false }, claimsIdentity);
+
+                    return RedirectToLocal(returnUrl);
+                }
+                catch (Exception e)
+                {
+                    ModelState.AddModelError("", e.Message);
+                }
+            }
+
+            return View(vm);
+        }
+
+        [Authorize]
+        public ActionResult Logout()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [Authorize]
+        public ActionResult Claims()
+        {
+            return View();
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
