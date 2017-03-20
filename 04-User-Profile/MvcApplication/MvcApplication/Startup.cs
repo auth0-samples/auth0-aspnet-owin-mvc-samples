2af05435c5f261bdcc2dcece303e092a3308f734
Jerrie Pelser <jerrie@jerriepelser.com>
2017-03-20 10:14:50 +0700

    Fix User Profile

--- 04-User-Profile/MvcApplication/MvcApplication/Startup.cs
+++ 04-User-Profile/MvcApplication/MvcApplication/Startup.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
+﻿using System.Configuration;
+using Auth0.Owin;
 using Microsoft.Owin;
+using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Owin;
 
@@ -13,21 +13,33 @@ namespace MvcApplication
     {
         public void Configuration(IAppBuilder app)
         {
+            // Configure Auth0 parameters
+            string auth0Domain = ConfigurationManager.AppSettings["auth0:Domain"];
+            string auth0ClientId = ConfigurationManager.AppSettings["auth0:ClientId"];
+            string auth0ClientSecret = ConfigurationManager.AppSettings["auth0:ClientSecret"];
+
+            // Set Cookies as default authentication type
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
-                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                AuthenticationType = CookieAuthenticationDefaults.AuthenticationType,
                 LoginPath = new PathString("/Account/Login")
             });
 
-            // Use a cookie to temporarily store information about a user logging in with a third party login provider
-            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
-
             // Configure Auth0 authentication
-            app.UseAuth0Authentication(
-                clientId: System.Configuration.ConfigurationManager.AppSettings["auth0:ClientId"],
-                clientSecret: System.Configuration.ConfigurationManager.AppSettings["auth0:ClientSecret"],
-                domain: System.Configuration.ConfigurationManager.AppSettings["auth0:Domain"]);
+            var options = new Auth0AuthenticationOptions()
+            {
+                Domain = auth0Domain,
+                ClientId = auth0ClientId,
+                ClientSecret = auth0ClientSecret,
 
+                // Save the tokens to claims
+                SaveIdToken = true,
+                SaveAccessToken = true,
+                SaveRefreshToken = true
+            };
+            options.Scope.Add("offline_access"); // Request a refresh_token
+            app.UseAuth0Authentication(options);
         }
     }
 }
