85fb96c4cddefb4ba7cef5293dbba442ff99ed50
Jerrie Pelser <jerrie@jerriepelser.com>
2017-03-20 09:49:41 +0700

    Login step

--- 01-Login/MvcApplication/MvcApplication/Controllers/AccountController.cs
+++ 01-Login/MvcApplication/MvcApplication/Controllers/AccountController.cs
@@ -1,24 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
 
 namespace MvcApplication.Controllers
 {
     public class AccountController : Controller
     {
-        public ActionResult Login()
+        public ActionResult Login(string returnUrl)
         {
-            return View();
+            return new ChallengeResult("Auth0", returnUrl ?? Url.Action("Index", "Home"));
         }
 
         [Authorize]
-        public ActionResult Logout()
+        public void Logout()
         {
-            HttpContext.GetOwinContext().Authentication.SignOut();
-
-            return RedirectToAction("Index", "Home");
+            HttpContext.GetOwinContext().Authentication.SignOut(CookieAuthenticationDefaults.AuthenticationType);
+            HttpContext.GetOwinContext().Authentication.SignOut(new AuthenticationProperties
+            {
+                RedirectUri = Url.Action("Index", "Home")
+            }, "Auth0");
         }
 
         [Authorize]
@@ -27,4 +28,36 @@ namespace MvcApplication.Controllers
             return View();
         }
     }
+
+    internal class ChallengeResult : HttpUnauthorizedResult
+    {
+        private const string XsrfKey = "XsrfId";
+
+        public ChallengeResult(string provider, string redirectUri)
+            : this(provider, redirectUri, null)
+        {
+        }
+
+        public ChallengeResult(string provider, string redirectUri, string userId)
+        {
+            LoginProvider = provider;
+            RedirectUri = redirectUri;
+            UserId = userId;
+        }
+
+        public string LoginProvider { get; set; }
+        public string RedirectUri { get; set; }
+        public string UserId { get; set; }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var properties = new AuthenticationProperties { RedirectUri = RedirectUri };
+            if (UserId != null)
+            {
+                properties.Dictionary[XsrfKey] = UserId;
+            }
+            context.HttpContext.GetOwinContext().Authentication.Challenge(properties, LoginProvider);
+        }
+    }
+
 }
