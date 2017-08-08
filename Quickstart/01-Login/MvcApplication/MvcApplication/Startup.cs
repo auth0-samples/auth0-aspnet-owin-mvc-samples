e1a5012b07d881d486657e42bac99e0051cc371b
Jerrie Pelser <jerrie@jerriepelser.com>
2017-08-08 11:53:59 -0500

    Add commented code on how to pass audience

--- Quickstart/01-Login/MvcApplication/MvcApplication/Startup.cs
+++ Quickstart/01-Login/MvcApplication/MvcApplication/Startup.cs
@@ -1,4 +1,6 @@
 ﻿using System.Configuration;
+using System.Linq.Expressions;
+using System.Net;
 using Auth0.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
@@ -32,8 +34,22 @@ namespace MvcApplication
                 Domain = auth0Domain,
                 ClientId = auth0ClientId,
                 ClientSecret = auth0ClientSecret,
+
+                // If you want to save tokens, then please uncomment the code below
+                // SaveAccessToken = true,
+
+                // If you want to request an access_token to pass to an API, then uncomment the code below
+                // Provider = new Auth0AuthenticationProvider
+                // {
+                //     OnApplyRedirect = context =>
+                //     {
+                //         context.RedirectUri += "&audience=" + WebUtility.UrlEncode("YOUR_API_IDENTIFIER");
+
+                //         context.Response.Redirect(context.RedirectUri);
+                //     }
+                // }
             };
-            options.Scope.Add("offline_access"); // Request a refresh_token
+            options.Scope.Add("openid profile"); // Request a refresh_token
             app.UseAuth0Authentication(options);
         }
     }
