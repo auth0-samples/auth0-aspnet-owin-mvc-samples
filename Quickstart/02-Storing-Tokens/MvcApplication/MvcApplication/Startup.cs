cecc29effde4022dc2833651b913516c31c6bc52
Rafael Noguera <rafanog2155@gmail.com>
2017-12-13 15:42:34 -0400

    added options to request refresh token
    Added two options to include the storing of refresh token

--- Quickstart/02-Storing-Tokens/MvcApplication/MvcApplication/Startup.cs
+++ Quickstart/02-Storing-Tokens/MvcApplication/MvcApplication/Startup.cs
@@ -43,6 +43,7 @@ namespace MvcApplication
                 // Save the tokens to claims
                 SaveIdToken = true,
                 SaveAccessToken = true,
+                SaveRefreshToken = true,
 
                 // If you want to request an access_token to pass to an API, then replace the audience below to 
                 // pass your API Identifier instead of the /userinfo endpoint
@@ -57,6 +58,8 @@ namespace MvcApplication
                     }
                 }
             };
+            options.Scope.Add("offline_access"); // Request a refresh_token
+            
             app.UseAuth0Authentication(options);
         }
     }
