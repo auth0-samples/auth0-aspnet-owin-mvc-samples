882168286177288e5b3a11f4d9bcd2a3f4ac53d7
Frederik <frederik.prijck@gmail.com>
2022-08-09 10:26:03 +0200

    Fix 02-UserProfile dependencies

--- Quickstart/02-User-Profile/MvcApplication/MvcApplication/Scripts/_references.js
+++ Quickstart/02-User-Profile/MvcApplication/MvcApplication/Scripts/_references.js
@@ -1,5 +1,5 @@
-﻿/// <reference path="modernizr-2.8.3.js" />
-/// <reference path="jquery-3.5.1.js" />
+﻿/// <reference path="jquery-3.6.0.js" />
+/// <reference path="modernizr-2.8.3.js" />
 /// <autosync enabled="true" />
 /// <reference path="bootstrap.min.js" />
 /// <reference path="jquery.validate.min.js" />
